278a7f303c4d9c046c93862f4a02f27305b024fa
doge3310 <65708897+doge3310@users.noreply.github.com>
2025-11-08 10:42:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from peewee_model import User
+from tkinter import ttk
 
 
 class App(tk.Frame):
@@ -7,8 +8,21 @@ class App(tk.Frame):
         super().__init__(master)
         self.pack()
 
-        self.text = tk.Text()
-        self.text.pack()
+        columns = ["1", "2", "3"]
+        self.treeview = ttk.Treeview(columns=columns, show="headings")
+        self.treeview.heading("1", text="id")
+        self.treeview.heading("2", text="username")
+        self.treeview.heading("3", text="password")
+        self.treeview.column("1", width=30, anchor="center")
+        self.treeview.column("2", width=100, anchor="center")
+        self.treeview.column("3", width=350, anchor="center")
+        self.treeview.pack(fill=tk.BOTH, expand=True)
+
+        # self.username = tk.Entry()
+        # self.password.pack()
+
+        # self.password = tk.Entry()
+        # self.password.pack()
 
         self.button = tk.Button(
             text="сделать заебись",
@@ -18,11 +32,10 @@ class App(tk.Frame):
 
     def view(self):
         for item in User.select():
-            self.text.insert(tk.END, (item.id, item.username, item.password, "\n"))
+            self.treeview.insert("", 0, values=[item.id, item.username, item.password])
 
 
 root = tk.Tk()
 root.geometry("500x500")
-root.resizable(False, False)
 myapp = App(root)
 myapp.mainloop()
